bbe64c9d7925f8440610fd0c069b591b8826f5a8
kaue gatto <kauegatto123@gmail.com>
2024-03-10 19:33:35 -0300

    feat: fix controller & add reqeusts

--- customer-ms/src/main/java/org/kaue/customer/WebAPI/Controllers/CustomerController.java
+++ customer-ms/src/main/java/org/kaue/customer/WebAPI/Controllers/CustomerController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/customers/")
+@RequestMapping("api/v1/customers")
 @Slf4j
 public record CustomerController (CustomerService service) {
 
